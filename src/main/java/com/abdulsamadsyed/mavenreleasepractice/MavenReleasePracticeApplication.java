970208d4612da45ae2723f1a90c4ef56ca09c4b3
Abdul <abdul.samad.apps@gmail.com>
2021-06-22 11:54:51 +0300

    Added initial basic files.

---
+++ src/main/java/com/abdulsamadsyed/mavenreleasepractice/MavenReleasePracticeApplication.java
@@ -0,0 +1,15 @@
+package com.abdulsamadsyed.mavenreleasepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReleasePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenReleasePracticeApplication.class, args);
+        System.out.println("Hello maven release practice");
+
+    }
+
+}
---
+++ src/test/java/com/abdulsamadsyed/mavenreleasepractice/MavenReleasePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulsamadsyed.mavenreleasepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenReleasePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
